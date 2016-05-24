aa27e049d55e8c56ac4a845e85ea912a3e30774a
edgarriba <edgar.riba@gmail.com>
2016-05-24 14:37:33 +0200

    add testing framework

---
+++ test/test_bot.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestUM(unittest.TestCase):
+
+	def setUp(self):
+		pass
+	
+	def test_01(self):
+		self.assertTrue(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
